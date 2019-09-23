fe612d56415260e88488a0d7d79ad46cba9c26ab
devduttjangir <devdutt.jangir@3pillarglobal.com>
2019-09-23 19:14:45 +0530

    base structure updated

---
+++ src/Screens/Root.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native'
+import Home from './Home';
+const Root = _ => (
+  <SafeAreaView>
+    <Home />
+  </SafeAreaView>
+);
+export default Root;
