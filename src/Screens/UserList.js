f255324ca45cfc1971ea3e7d8f0de159a27d361c
Devdutt <devpassion7@gmail.com>
2019-10-12 10:03:43 +0530

    user list added

--- src/Screens/UserList.js
+++ src/Screens/UserList.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import {Text, View, FlatList, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+} from 'react-native';
 
 export default class UserList extends Component {
   constructor(props) {
@@ -7,16 +14,40 @@ export default class UserList extends Component {
     this.state = {
       users: [
         {
-          id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-          title: 'First Item',
+          id: '2',
+          name: 'Naresh Kumar',
+          emailAddress: 'nresh@gmail.com',
+          mobilenumber: '77777777',
+          meet_to: 'Dev',
+          reason: 'office',
+          no_of_person: '3',
+          address: 'Gurgaon',
+          visiting_date: '2019-09-08 02:26:40',
+          images: '5d731560ee1b4.jpg',
         },
         {
-          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-          title: 'Second Item',
+          id: '3',
+          name: 'Ajay Kumar',
+          emailAddress: 'Ajay@gmail.com',
+          mobilenumber: '88888888',
+          meet_to: 'Rajesh',
+          reason: 'office',
+          no_of_person: '2',
+          address: 'Gurgaon',
+          visiting_date: '2019-09-08 02:34:22',
+          images: '5d73172dedac0.jpg',
         },
         {
-          id: '58694a0f-3da1-471f-bd96-145571e29d72',
-          title: 'Third Item',
+          id: '4',
+          name: 'Ajay Kumar',
+          emailAddress: 'Ajay@gmail.com',
+          mobilenumber: '88888888',
+          meet_to: 'tesr',
+          reason: 'visit',
+          no_of_person: '2',
+          address: 'Gurgaon',
+          visiting_date: '2019-09-08 11:11:10',
+          images: '5d74e1ce2a08e.jpg',
         },
       ],
     };
@@ -25,8 +56,32 @@ export default class UserList extends Component {
   listItem = item => {
     return (
       <View>
-        <Text>{item.id}</Text>
-        <Text>{item.title}</Text>
+        <View style={styles.rowContainer}>
+          <View style={styles.imageContainer}>
+            <Image
+              style={{width: 80, height: 80}}
+              source={{
+                uri:
+                  'https://facebook.github.io/react-native/img/tiny_logo.png',
+              }}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textStyle}>{item.name}</Text>
+            <Text>{item.visiting_date}</Text>
+          </View>
+        </View>
+
+        <View style={styles.rowContainer}>
+          <Text>{item.emailAddress}</Text>
+          <Text>{item.mobilenumber}</Text>
+        </View>
+        <View style={styles.rowContainer}>
+          <Text>{item.meet_to}</Text>
+          <Text>{item.no_of_person}</Text>
+        </View>
+        <Text>{item.address}</Text>
+        <Text>{item.reason}</Text>
       </View>
     );
   };
@@ -47,4 +102,13 @@ export default class UserList extends Component {
 
 const styles = StyleSheet.create({
   listContainer: {},
+  cellRowContainer: {
+    padding: 16,
+  },
+  imageContainer: {},
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textStyle: {},
 });
